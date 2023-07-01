c07027169205d0c5551257548b7e8f38ee5fa40e
David Mei <helloapplejuice123@gmail.com>
2023-07-01 13:33:04 -0500

    practice question 1-3

---
+++ week3.js
@@ -0,0 +1,15 @@
+const yes = "haha"
+
+console.log(yes)
+
+let length = 10
+
+console.log(length + 15)
+
+//question 3
+
+const studentId = "12380994"
+const studentName = "David"
+const schoolName = "Mather"
+
+console.log("My name is " + studentName + ". My student ID is " + studentId + ". My school name is " + schoolName + ".")

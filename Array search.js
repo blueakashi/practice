749301b9419d7888cb3e4642e52e34747d749f5f
blueakashi <137646510+blueakashi@users.noreply.github.com>
2023-07-21 17:59:55 -0500

    Update Array search.js

--- Array search.js
+++ Array search.js
@@ -1,11 +1,39 @@
 const numbers = [13, 12, 15, 24, 23]
 
+//FINd FIRST NUMBER INdEX
+
 function searchEvenNumber(array) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] % 2 === 0) {
             return i; 
         } }
-        return -1
+         return -1
     }
 
+//FINd ALL EVEN NUMBERS
+
+function allEvenNumbers(array) {
+    const evens = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evens.push(i)
+        }
+    }
+    return evens
+}
+
+//FINd ALL EVEN NUMBERS (Join method)
+
+function allEvenNumbers2(array) {
+    const evens = []
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] % 2 === 0) {
+            evens.push(i)
+        }
+    }
+    return evens.join(', ')
+}
+
 console.log(searchEvenNumber(numbers))
+console.log(allEvenNumbers(numbers))
+console.log(allEvenNumbers2(numbers))
